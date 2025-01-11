310c4eb1ace91de6b0a7f16280a81a6ea1a845e0
itIsMuse <oyeniranshock@gmail.com>
2025-01-11 07:18:10 +0100

    chore: Update Tailwind CSS configuration and add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Containers/Header'
 
 function App() {
 
 
   return (
    <div>
-    <h1>Hello World</h1>
+    <Header/>
+    <h1 className='text-4xl font-bold text-white-500' >Hello World</h1>
    </div>
   )
 }
---
+++ src/Containers/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default Header
