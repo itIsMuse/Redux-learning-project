d57cb47fe9599928dce0625dd2a1dba5f21b2de9
itIsMuse <oyeniranshock@gmail.com>
2025-01-16 07:38:47 +0100

    chore: Update dependencies and add axios for API requests

--- src/Containers/Product.jsx
+++ src/Containers/Product.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Product = () => {
+  const products = useSelector((state) => state.allProducts.products)
+  const { id, title } = products[0]
+  console.log(products)
   return (
-    <div>
-      <h1>Product</h1>
-    </div>
+    <div className='col-4 bg-gray-200 p-4'>
+      <a href="#" className ="block bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <img src="image-url.jpg" alt="Card Image" className ="w-full h-48 object-cover rounded-t-lg"/>
+  <div className ="p-4">
+      <h2 className ="text-lg font-semibold">{title}</h2>
+      <p className ="text-gray-600"></p>
+  </div>
+</a>
+  </div>
   )
 }
 
--- src/Containers/ProductListings.jsx
+++ src/Containers/ProductListings.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
 import { useSelector } from 'react-redux'
+import Product from '../Containers/Product'
 
 const ProductListings = () => {
 
   const products = useSelector(state => state)
   console.log(products)
 
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      console.log(response.data);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className='mt-20'>
-     <h1>Product listings</h1>
+    <div className='mt-20 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+     <Product />
     </div>
   )
 }
