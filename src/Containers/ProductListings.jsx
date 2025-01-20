765cbdf1ee5e4a20343e11f80a8ffd96a2ed6065
itIsMuse <oyeniranshock@gmail.com>
2025-01-20 07:39:55 +0100

    added the setloading feature and add react-spinners package

--- src/Containers/ProductListings.jsx
+++ src/Containers/ProductListings.jsx
@@ -1,21 +1,25 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import { useSelector, useDispatch} from 'react-redux'
 import Product from '../Containers/Product'
 import {setProducts} from '../Redux/Actions/productAction.js'
+import  ClipLoader  from 'react-spinners/ClipLoader.js'
 
 const ProductListings = () => {
 
 
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true); // Add a loading state
   
 
   const fetchProducts =  async() => {
     try {
       const response = await axios.get('https://fakestoreapi.com/products');
       dispatch(setProducts(response.data));
+      setLoading(false); // Set loading to false after the data is fetched
     } catch (error) {
       console.log('Error:', error);
+      setLoading(false); // Set loading to false if there is an error
     }
   };
 
@@ -23,6 +27,14 @@ const ProductListings = () => {
     fetchProducts();
 
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <ClipLoader color="#4A90E2" size={100} /> {/* Spinner */}
+      </div>
+    );
+  }
 
   return (
     <div>
