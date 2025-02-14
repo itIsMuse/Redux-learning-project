9ed33a6cc22e3c746cd0f71d6b33bd2d901777f6
itIsMuse <oyeniranshock@gmail.com>
2025-02-14 06:25:29 +0100

    feat: Add user reducer to combined reducers and configure Redux store

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 import productReducer from "./productReducers";
+import userReducer from "./userReducers";
+import { use } from "react";
+import cartSlice from "../cartSlice";
 
 export const reducers = combineReducers({
     allProducts: productReducer,
+    user: userReducer,  // Add the user reducer to the combined reducers
+    cart: cartSlice
     
 })
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,25 +1,22 @@
-import { createStore } from 'redux';
-import {reducers} from './Reducers/index'; // Import the reducer from src/Redux/Reducers/index.js
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
+import { reducers } from "./Reducers/index.js"; // Your existing combined reducers
 
-const store = createStore(reducers,{}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); // Create a Redux store holding the state of your app.  
-// Its API is { subscribe, dispatch, getState }.                    
-// It's important to note that you'll only have a single store in a Redux application.  
+export const store = configureStore({
+  reducer: reducers,
+  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools only in development
+});
 
-export default store; // Export the store so that it can be used by the rest of the application.
-
-
+export default store;
 // import { configureStore } from "@reduxjs/toolkit";
-// import cartReducer from "./cartSlice";
-// import { reducers } from "./Reducers/index"; // Your existing combined reducers
+// import {reducers} from './Reducers/index'; // Import the reducer from src/Redux/Reducers/index.js
 
 // export const store = configureStore({
-//   reducer: {
-//     ...reducers, // Include all your existing reducers
-//     cart: cartReducer, // Add the cart slice reducer
-//   },
-//   devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools only in development
-// });
+//     reducer: reducers, 
+//     devTools: process.env.NODE_ENV !== "production", // Enables Redux DevTools only in development
+//   });  
 
-// export default store;
 
-// compare this snippet from src/Redux/Actions/productAction.js:
+// export default store; 
+
+
